9f01fa7d800a37ffe8c9b77cd4cb32c40e7fe027
Chaewon <cw001121@naver.com>
2023-01-14 22:25:33 +0900

    add READMe.md

--- 외계어_사전/noah/alien.py
+++ 외계어_사전/noah/alien.py
@@ -8,11 +8,13 @@ def solution(spell, dic):
             if count == len(spell):
                 result += 1
         count = 0
+
     if result == 0:
         return 2
     else:
         return 1
-        
+
+
 dic = ["def", "dww", "dzx", "loveaw"]
 spell = ["z", "d", "x"]
-print(solution(spell,dic))
+print(solution(spell, dic))
