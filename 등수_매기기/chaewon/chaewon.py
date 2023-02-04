40df348632eb8929bddd30734be2a3a6be30acd8
Chaewon <cw001121@naver.com>
2023-02-04 16:58:08 +0900

    [Programmers-6 week] chaewon

--- 등수_매기기/chaewon/chaewon.py
+++ 등수_매기기/chaewon/chaewon.py
@@ -5,14 +5,14 @@ def solution(score):
     before_avg_score = 0
 
     for rank, avg_score in enumerate(avg_score_list):
-      present_ind = score.index(avg_score) # score에서의 기존 index
+      present_ind = score.index(avg_score)
       present_avg_score = sum(avg_score)/2
 
       if before_avg_score == present_avg_score:
         ranking[present_ind] = ranking[before_ind]
 
       else:
-        ranking[present_ind] = rank + 1 # 등수 부여
+        ranking[present_ind] = rank + 1
 
       before_avg_score = present_avg_score
       before_ind = present_ind
