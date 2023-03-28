817e8781c09d8bd5e2951b58b0f7ae5811bf6abf
Chaewon <cw001121@naver.com>
2023-03-28 13:41:06 +0900

    [Programmers-12 week] chaewon

--- 평행/chaewon/chaewon.py
+++ 평행/chaewon/chaewon.py
@@ -9,7 +9,7 @@ def solution(dots):
       gradient.append(get_gradient(combin))
 
     ## 기울기 중 0 제거
-    gradient = [i for i in gradient if i != 0]
+    gradient = [i for i in gradient if i]
 
     if len(gradient) == len(set(gradient)):
       return 0
@@ -22,8 +22,8 @@ def get_gradient(dots):
   if not a2 - a1 == 0:
     gradient = (b2 - b1) / (a2 - a1)
   else:
-    gradient = 0
+    gradient = None
   return gradient
 
 print(solution([[1, 4], [9, 2], [3, 8], [11, 6]]))
-print(solution([[3, 5], [4, 1], [2, 4], [5, 10]]))
+# print(solution([[3, 5], [4, 1], [2, 4], [5, 10]]))
