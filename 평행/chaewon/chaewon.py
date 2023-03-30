bea1ecdffe978151130e005df182ecf72f806854
Chaewon <cw001121@naver.com>
2023-03-30 16:14:23 +0900

    [Programmers-12 week] chaewon

--- 평행/chaewon/chaewon.py
+++ 평행/chaewon/chaewon.py
@@ -1,14 +1,13 @@
 from itertools import combinations
 
 def solution(dots):
-    answer = 0
     gradient = []
     combination = list(combinations(dots, 2))
 
     for combin in combination:
       gradient.append(get_gradient(combin))
 
-    ## 기울기 중 0 제거
+    # 기울기 중 0 제거
     gradient = [i for i in gradient if i]
 
     if len(gradient) == len(set(gradient)):
@@ -19,11 +18,7 @@ def solution(dots):
 def get_gradient(dots):
   a1, b1 = dots[0]
   a2, b2 = dots[1]
-  if not a2 - a1 == 0:
-    gradient = (b2 - b1) / (a2 - a1)
-  else:
-    gradient = None
-  return gradient
+  return (b2 - b1) / (a2 - a1)
 
 print(solution([[1, 4], [9, 2], [3, 8], [11, 6]]))
 # print(solution([[3, 5], [4, 1], [2, 4], [5, 10]]))
--- 평행/noah/noah.py
+++ 평행/noah/noah.py
@@ -1,12 +1,12 @@
 def inclination(list1, list2):
     x1, y1 = list1
     x2, y2 = list2
-    return (x2 - x1)/(y2 - y1) 
+    return (x2 - x1)/(y2 - y1)
 
 
 def solution(dots):
     a, b, c, d = dots
-    if inclination(a,b) == inclination(c, d) or\
+    if inclination(a,b) == inclination(c, d) or \
         inclination(a,c) == inclination(b, d) or \
         inclination(a,d) == inclination(b, c):
         return 1
